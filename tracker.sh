c84c610a5428298d901b12188342d78df1cd0c5d
paavo <shibaev.pp@phystech.edu>
2022-02-09 04:01:36 +0300

    initial

---
+++ tracker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ssh root@10.0.0.1
+file=$1;
+## shellcheck disable=SC2034
+#while ( (i=7;i<=12;i++) ); do
+#  sleep 3s;
+#  rssi=`iw dev wlan0 station dump | grep "signal:" | tail -n 1 | awk '{print $2}'`;
+#  echo $rssi >> $file;
+#done
+for ((i=1;i<=50;i++)); do
+  echo $(ssh root@10.0.0.1 iw dev wlan0 station dump | grep "signal:" | tail -n 1 | awk '{print $2}') >> $file
+  sleep 2s;
+done
