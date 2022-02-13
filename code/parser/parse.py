815d0707bcb7e3ffa34ba644a550886aa4a5bd75
paavo <shibaev.pp@phystech.edu>
2022-02-13 20:09:28 +0300

    update parser

--- code/parser/parse.py
+++ code/parser/parse.py
@@ -1,10 +1,15 @@
-from typing import List
-
+# from typing import List
+import pandas as pd
 
 class Parser:
     @classmethod
-    def read(cls, file: str) -> List[int]:
+    def parse_line(s: str) -> (float, int):
+        i, j = s.split(",")
+        return float(i), int(j)
+
+    @classmethod
+    def read(cls, file: str) -> pd.DataFrame:
         with open(file, "r") as f:
-            values = list(map(int, f.readlines()))
-            return values
+            values = list(map(Parser.parse_line, f.readlines()))
+            return pd.DataFrame(values)
 
