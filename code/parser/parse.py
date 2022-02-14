64b6da09af40ef63bd0ac5e3fea59ddf7e810a11
paavo <shibaev.pp@phystech.edu>
2022-02-14 20:13:27 +0300

    add trap

--- code/parser/parse.py
+++ code/parser/parse.py
@@ -3,13 +3,14 @@ import pandas as pd
 
 class Parser:
     @classmethod
-    def parse_line(s: str) -> (float, int):
+    def parse_line(cls, s: str) -> (float, int):
         i, j = s.split(",")
         return float(i), int(j)
 
     @classmethod
     def read(cls, file: str) -> pd.DataFrame:
         with open(file, "r") as f:
-            values = list(map(Parser.parse_line, f.readlines()))
+            values = [Parser.parse_line(l) for l in f.readlines()]
+            # values = list(map(Parser.parse_line, f.readlines()))
             return pd.DataFrame(values)
 
--- tracker.sh
+++ tracker.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+        echo "** Trapped CTRL-C"
+        exit 0
+}
+
 ssh root@10.0.0.1
 file=$1;
 ## shellcheck disable=SC2034
@@ -8,6 +17,7 @@ file=$1;
 #  echo $rssi >> $file;
 #done
 for ((i=1;i<=400;i++)); do
-  echo $(ssh root@10.0.0.1 iw dev wlan0 station dump | grep "signal:" | tail -n 1 | awk '{print $2}') >> $file
+  # echo $(ssh root@10.0.0.1 iw dev wlan0 station dump | grep "signal:" | tail -n 1 | awk '{print $2}') >> $file
+  echo action
   sleep 2s;
 done
