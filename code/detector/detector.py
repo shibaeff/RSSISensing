de7094b26a689afbb8fa6f1292c40ee5d7fc5290
paavo <shibaev.pp@phystech.edu>
2022-02-17 16:15:13 +0300

    render image (finally)

--- code/detector/detector.py
+++ code/detector/detector.py
@@ -1,19 +1,23 @@
 import numpy as np
+
+
 class Detector():
-    def __init__(self, window_sz=5):
+    def __init__(self, window_sz=5, thresh=1.5):
         self._window_sz = window_sz
+        self._thresh = thresh
 
-    @classmethod
-    def is_radical(cls, v1, v2):
-        change = (v1/v2) if v1 >= v2 else (v2/v1)
-        return change >= 1.5
+    def is_radical(self, v1, v2):
+        change = (v1 / v2) if v1 >= v2 else (v2 / v1)
+        return change >= self._thresh
 
-    @classmethod
-    def predict(cls, dots):
-        return Detector.is_radical(np.mean(dots[1:]), np.mean(dots[:-1])) and Detector.is_radical(np.std(dots[1:]),
-                                                                                                  np.std(dots[:-1]))
+    def predict(self, dots):
+        return self.is_radical(np.mean(dots[1:]), np.mean(dots[:-1]))
+    #
+    # and self.is_radical(np.std(dots[1:]),
+    #                     np.std(dots[:-1]))
 
     def detect(self, inp):
         out = []
         for i in range(self._window_sz, len(inp), 1):
-            out.append(Detector.predict(out[i - self._window_sz: i]))
+            out.append(self.predict(inp[i - self._window_sz: i]))
+        return out
